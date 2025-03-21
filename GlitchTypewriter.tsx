c710a1d42bc31a1c27642c65c80cafbc7dbe090d
sunbyte <kostadin.tonev@mail.ru>
2025-03-21 18:53:35 +0200

    Add files via upload

---
+++ GlitchTypewriter.tsx
@@ -0,0 +1,126 @@
+import { useEffect, useRef } from "react"
+import "./GlitchTypewriter.css"
+
+interface GlitchTypewriterProps {
+  slogans: string[]
+  delay?: number
+  typeSpeed?: number
+  color?: string
+  fontSize?: number
+  fontWeight?: string
+  fontFamily?: string
+  showCursor?: boolean
+  cursorWidth?: number
+  cursorHeight?: number
+  cursorBackgroundColor?: string
+  cursorBlinkSpeed?: number
+}
+
+const randomChars =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(){}[]<>.,;:"
+
+const GlitchTypewriter = ({
+  slogans,
+  delay,
+  typeSpeed,
+  color,
+  fontSize,
+  fontWeight,
+  fontFamily,
+  showCursor,
+  cursorWidth,
+  cursorHeight,
+  cursorBackgroundColor,
+  cursorBlinkSpeed,
+}: GlitchTypewriterProps) => {
+  const sloganRef = useRef<HTMLSpanElement>(null)
+  const currentIndex = useRef<number>(0)
+
+  useEffect(() => {
+    const sloganElement = sloganRef.current
+    if (!sloganElement) return
+
+    const toggleClass = (
+      element: HTMLElement,
+      className: string,
+      add: boolean
+    ) => {
+      element.classList[add ? "add" : "remove"](className)
+    }
+
+    const getRandomIndex = (max: number) => Math.floor(Math.random() * max)
+
+    const typewriterEffect = () => {
+      if (!sloganElement) return
+      toggleClass(sloganElement, "glitch", false)
+
+      const currentText = slogans[currentIndex.current]
+      const textLength = currentText.length
+      let currentChar = 0
+
+      const interval = setInterval(
+        () => {
+          const displayText = [...currentText]
+            .map((char, index) =>
+              index < currentChar
+                ? char
+                : randomChars[getRandomIndex(randomChars.length)]
+            )
+            .join("")
+
+          sloganElement.textContent = displayText
+          currentChar++
+
+          if (currentChar > textLength) {
+            clearInterval(interval)
+            toggleClass(sloganElement, "glitch", true)
+            sloganElement.dataset.text = displayText
+            currentIndex.current = (currentIndex.current + 1) % slogans.length
+          }
+        },
+        typeSpeed ? typeSpeed : 15
+      )
+    }
+
+    typewriterEffect()
+    const loop = setInterval(typewriterEffect, delay ? delay : 3000)
+    return () => clearInterval(loop)
+  }, [])
+
+  const firstSlogan = slogans.length > 0 ? slogans[0] : ""
+
+  return (
+    <div className="slogan-container">
+      <span
+        ref={sloganRef}
+        className="slogan glitch"
+        data-text={firstSlogan}
+        style={{
+          color: color ? color : "black",
+          fontSize: fontSize ? fontSize : 18,
+          fontWeight: fontWeight ? fontWeight : "normal",
+          fontFamily: fontFamily ? fontFamily : "inherit",
+        }}
+      >
+        {firstSlogan}
+      </span>
+      {showCursor && (
+        <div
+          className="cursor"
+          style={{
+            width: cursorWidth ? cursorWidth : 4,
+            height: cursorHeight ? cursorHeight : 18,
+            backgroundColor: cursorBackgroundColor
+              ? cursorBackgroundColor
+              : "aqua",
+            animation: cursorBlinkSpeed
+              ? cursorBlinkSpeed + "s infinite blink"
+              : "0.7s infinite blink",
+          }}
+        />
+      )}
+    </div>
+  )
+}
+
+export default GlitchTypewriter
